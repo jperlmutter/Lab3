a5a35f60d4573263013c4a25dfd59c75825ab092
Lei Liu <leichrisliu@hotmail.com>
2016-09-17 15:09:47 -0500

    Add files via upload

--- alarmSwitch.c
+++ alarmSwitch.c
@@ -1,6 +1,17 @@
 #include <stdint.h>
+#include <stdio.h>
 #include "alarmSwitch.h"
 #include "../inc/tm4c123gh6pm.h"
+#include "ST7735.h"
+
+#define PE1   (*((volatile uint32_t *)0x4005C008))
+#define PE2   (*((volatile uint32_t *)0x4005C010))
+#define PE3   (*((volatile uint32_t *)0x4005C020))
+#define PE4   (*((volatile uint32_t *)0x4005C040))
+uint8_t alarm_Set=0;
+int32_t alarm_Minutes=1;
+int32_t alarm_Hours=1;
+
 
 void alarmSwitchInit(void){
 	
@@ -14,6 +25,45 @@ void alarmSwitchInit(void){
   GPIO_PORTE_DIR_R &= ~0x1E;         // 5) set direction to output
   GPIO_PORTE_AFSEL_R &= ~0x1E;      // 6) regular port function
   GPIO_PORTE_DEN_R |= 0x1E;         // 7) enable digital port
+	//GPIO_PORTE_IS_R &= ~0x1E;				//PE1-4 is edge sensitive
+}
+/*void GPIOPortE_Handler(void){
+	GPIO_PORTE_ICR_R = 0x01;					// Acknowledge flag
+}*/
+void display_Alarm(void){
+	ST7735_FillScreen(ST7735_BLACK);				//displays the time that the user is currently looking at to set
+	ST7735_SetCursor(0,0);
+	ST7735_OutString("Press key 2 to set alarm");
+	ST7735_SetCursor(0,1);
+	ST7735_OutString("Press key 3 to + time");
+	ST7735_SetCursor(0,2);
+	ST7735_OutString("Press key 4 to - time");
+	ST7735_SetCursor(0,3);
+	ST7735_OutString("Set alarm time: ");
+	ST7735_OutUDec(alarm_Hours);
+	ST7735_OutString(":");
+	ST7735_OutUDec(alarm_Minutes);
 }
 
-
+void set_Alarm(){
+	while(PE2!=0x00){
+		display_Alarm();
+		if(PE3==0x00){													//if PE3 (add) is pressed then minutes will be incremented
+			alarm_Minutes=(alarm_Minutes+1)%60;
+			if(alarm_Minutes==0){
+				alarm_Hours=(alarm_Hours+1)%24;
+			}
+		}
+		if(PE4==0x00){													//if PE4 (minus) is pressed then minutes will be decremented
+			alarm_Minutes-=1;
+			if(alarm_Minutes<0){
+				alarm_Hours-=1;
+				if(alarm_Hours<0){
+					alarm_Hours+=24;
+				}
+				alarm_Minutes+=60;
+			}
+		}
+	}
+	alarm_Set=1;
+}
